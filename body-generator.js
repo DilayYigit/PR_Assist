21318cfe79ed55ac216a4b51f61a1b87121b5afa
amir <amirrezaa.khoshbakht@gmail.com>
2024-04-06 10:24:13 +0300

    description generation works now

--- body-generator.js
+++ body-generator.js
@@ -1,4 +1,5 @@
 import {OpenAIClient, AzureKeyCredential} from '@azure/openai'
+import axios from 'axios'
 
 const endpoint = "https://amirtest.openai.azure.com/";
 const azureApiKey = '5a245cbd34374444a17808a1160b32d3';
@@ -7,15 +8,33 @@ export async function generateBody(context) {
 
     try {
         // Get the diff of the pull request
-        const diff = await context.octokit.pulls.get({
+        const info = await context.pullRequest({
             owner: context.payload.repository.owner.login,
             repo: context.payload.repository.name,
             pull_number: context.payload.pull_request.number
-        }).then(response => response.data.diff);
+        })
+        // console.log(info);
+        let diffFiles = null;
 
-        console.log(diff);
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${info.owner}/${info.repo}/pulls/${info.pull_number}/files`, {
+                headers: {
+                    'Accept': 'application/vnd.github.v3.diff',
+                }
+            });
 
-        const prompt = [`The changes in this pull request are:\n\n${diff}\n\nPlease provide a concise description of the changes.`];
+            diffFiles = response.data.map((file) => {
+                return file.patch;
+            });
+
+            // console.log(diffFiles)
+
+        } catch (error) {
+            console.error('Error getting diff files:', error);
+        }
+
+
+        const prompt = [`The changes in this pull request are:\n\n${diffFiles}\n\nPlease provide a concise description of the changes.`];
 
         const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
         const deploymentId = "gpt35Test";
@@ -26,7 +45,8 @@ export async function generateBody(context) {
         }
         return result.choices[0].text;
 
-    } catch (e) {
+    } catch
+        (e) {
         console.error("The sample encountered an error:", e);
 
 
