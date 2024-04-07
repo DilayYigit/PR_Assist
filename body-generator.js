7298600be22ae1aa9f05f92b4f81e8928c8cfdd1
amir <amirrezaa.khoshbakht@gmail.com>
2024-04-07 09:50:50 +0300

    fixed the text generation

--- body-generator.js
+++ body-generator.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const endpoint = "https://amirtest.openai.azure.com/";
 const azureApiKey = '5a245cbd34374444a17808a1160b32d3';
 
+
 export async function generateBody(context) {
 
     try {
@@ -34,24 +35,40 @@ export async function generateBody(context) {
         }
 
 
-        const prompt = [`The changes in this pull request are:\n\n${diffFiles}\n\nPlease provide a concise description and a title of the changes. Return a response in this format: Title: Description:`];
-        const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-        const deploymentId = "gpt35Test";
-        const result = await client.getCompletions(deploymentId, prompt);
 
-        for (const choice of result.choices) {
-            console.log(choice.text);
-        }
-        return result.choices[0].text;
+        const m = [
+            {
+                "role": "system",
+                "content": "Please generate a concise description and title for a pull request based on the provided diff files. Here's an example of your output format: \"Title\": \"\" \n \"Description\": "
+            },
+            {
+                "role": "user",
+                "content": `${diffFiles}`
+            }
+        ];
+
+
+        const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+        const deploymentId = "gpt35";
+        const result2 = await client.getChatCompletions(deploymentId, m);
+
+
+
+
+
+        console.log(result2.choices[0])
+        return result2.choices[0].message.content
+
 
     } catch
-        (e) {
-        console.error("The sample encountered an error:", e);
+        (e)
+        {
+            console.error("The sample encountered an error:", e);
+
+
+        }
 
 
     }
-
-
-}
 
 
