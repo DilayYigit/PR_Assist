4bc5969cf5def9af022310bd91492d482f41c9a5
amir <amirrezaa.khoshbakht@gmail.com>
2024-05-12 19:01:07 +0300

    small changes

--- suggest-reviewer.js
+++ suggest-reviewer.js
@@ -1,54 +1,51 @@
-import { Octokit } from "octokit";
+import {Octokit} from "octokit";
 import axios from 'axios';
 
 const octokit = new Octokit({
-    auth: 'YOUR_GITHUB_TOKEN' // TODO: REPLACE WITH YOUR GITHUB TOKEN
-  })
+    //
+    auth: '' // TODO: REPLACE WITH YOUR GITHUB TOKEN
+})
 
 export async function suggestReviewer(context) {
 
-    try {
-        const info = await context.pullRequest({
-            owner: context.payload.repository.owner.login,
-            repo: context.payload.repository.name,
-            pull_number: context.payload.pull_request.number
-        })
 
-        try {
-            const response = await octokit.request(`GET /repos/${info.owner}/${info.repo}/activity`, {
-                headers: {
-                    'Accept': 'application/vnd.github+json',
-                }
-            });
+    const info = await context.pullRequest({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        pull_number: context.payload.pull_request.number
+    })
 
-            //console.log(response.data);
-            const len = response.data.length;
-            let contributor = '';
-            for (let i = 0; i < len; i++) {
-                if (response.data[i].activity_type === 'push') {
-                    contributor = response.data[i].actor.login;
-                    break;
-                }   
-            }
-        
-            //console.log(contributor);
-            const response2 = await octokit.request(`POST /repos/${info.owner}/${info.repo}/pulls/${info.pull_number}/requested_reviewers`, {
-                reviewers: [
-                    contributor
-                ],
-                headers: {
-                    'Accept': 'application/vnd.github+json',
-                }
-            });
-            //console.log(response2.data);
-            return response2.status;
 
-        } catch (error) {
-            console.error('Error getting the activity:', error);
+    const response = await octokit.request(`GET /repos/${info.owner}/${info.repo}/pulls/${info.pull_number}/files`, {
+        headers: {
+            'Accept': 'application/vnd.github.v3+json',
         }
-    } catch (error) {
-            console.error("Error with the GitHub Pull Request:", error);
-    }
+    });
+
+
+    const changedFiles = response.data;
+
+    let fileNames = [''];
+
+    changedFiles.forEach(file => {
+        fileNames.push(file.filename)
+    });
+
+    const response2 = await octokit.request(`GET /repos/${info.owner}/${info.repo}/contributors`, {
+        headers: {
+            'Accept': 'application/vnd.github.v3+json',
+        }
+    });
+    const contributors = response2.data;
+
+
+    // Create an object to store the line counts for each contributor
+    const contributorLineCount = {};
+    contributors.forEach(contributor => {
+        contributorLineCount[contributor.login] = {additions: 0, deletions: 0};
+    });
+
+
 }
 
 
